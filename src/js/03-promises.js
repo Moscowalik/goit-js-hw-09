bf790d9a68b3123b742261d64d1483ea712fa708
Moscowalik <moscowalik95@gmail.com>
2022-02-03 21:24:04 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,4 +1,18 @@
+import Notiflix from 'notiflix';
+
+const refs = {
+  delayInput: document.querySelector('[name="delay"]'),
+  stepInput: document.querySelector('[name="step"]'),
+  amountInput: document.querySelector('[name="amount" ]'),
+  btn: document.querySelector('button'),
+};
+
+const position = 0;
+
+refs.btn.addEventListener('submit', createPromise);
+
 function createPromise(position, delay) {
+  position += 1;
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
     // Fulfill
